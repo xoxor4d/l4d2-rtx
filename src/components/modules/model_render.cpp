c63f76bd883921366595dbf36d07b29d755a5376
#xoxor4d <r4d0xzz@live.de>
2025-08-08 09:57:46 +0200

    Fix some l4d1 infected models not using the infected shader path

--- src/components/modules/model_render.cpp
+++ src/components/modules/model_render.cpp
@@ -486,7 +486,8 @@ namespace components
 			}
 
 			if (ctx.info.shader_name == "Infected" && 
-				(ctx.info.material_name.contains("/l4d2/") || ctx.info.material_name.contains("/l4d1/cim_ceda")))
+				(ctx.info.material_name.contains("/l4d2/") || 
+				 ctx.info.material_name.contains("/l4d1/cim_"))) 
 			{
 				//ctx.modifiers.do_not_render = true;
 
